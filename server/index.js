368171361d924222725d8c33806815f26f716677
Reese Schumacher <reesesch1@gmail.com>
2021-07-20 11:23:32 -0600

    added rollbar

--- server/index.js
+++ server/index.js
@@ -3,8 +3,20 @@ const path = require('path')
 
 const app = express()
 
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: 'f83506a893014d2eb782086d77531e5f',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
+
+
+
+
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../tictacjs.html'));
+  rollbar.info('served successfully!!!')
+  rollbar.log('Its working! ITS WORKING!!!')
 });
 
 const port = process.env.PORT || 4000
